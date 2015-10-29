092682a20b189b2fa1a4679f89c6b0dddaef7cc6
Lars <lars.lapp@gmail.com>
2015-10-29 21:02:29 +0000

    USE SSL and puma in prod

--- test/integration/user_signup_test.rb
+++ test/integration/user_signup_test.rb
@@ -15,11 +15,11 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
   
   test "valid signup information" do
     get signup_path
-    assert_difference 'User.count' do
-        post users_path, user: { name:  "Foobar",
-              email: "user@invalid.de",
-              password:              "foobar",
-              password_confirmation: "foobar" }
+    assert_difference 'User.count', 1 do
+      post_via_redirect users_path, user: { name:  "Example User",
+                                            email: "user@example.com",
+                                            password:              "password",
+                                            password_confirmation: "password" }
     end
     assert_template 'users/show'
   end
